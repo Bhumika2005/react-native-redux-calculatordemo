5ad908deac523b7753cf439a24d0556ce4c4509a
mbhumika <bhumika.mehta@sooryen.com>
2018-03-12 12:46:08 +0530

    update

--- app/reducers/CounterReducer.js
+++ app/reducers/CounterReducer.js
@@ -11,8 +11,9 @@ const initialState={
 const CounterReducer= (state=initialState,action)=>{
     switch(action.type){
         case Actions.OPRATOR_KEY:
-        if(state.displayValue!=='' && state.selectedOperator === null)  {
+        if(state.displayValue!=='' && state.selectedOperator === null && state.displayValue !== Actions.DIVIDE_BY_ZERO)  {
             if(state.displayValue.substr(-1) !== '.'){
+                
                 return Object.assign({},state,{
                     previouseInputVal:state.inputValue,
                     selectedOperator:action.val,
@@ -20,6 +21,7 @@ const CounterReducer= (state=initialState,action)=>{
                     inputValue:0,
                     lastOperator:action.val
                  });
+                
             }
             else{
                 return state;
@@ -29,17 +31,28 @@ const CounterReducer= (state=initialState,action)=>{
             return state;
         }
         case Actions.EQAUL:
-            if(state.displayValue != '' && state.selectedOperator !== null){
+            if(state.displayValue != '' && state.displayValue !== Actions.DIVIDE_BY_ZERO && state.selectedOperator !== null){
                 if(this.isValid(state)){
-                    let answer=eval(parseFloat(state.previouseInputVal)+state.selectedOperator+parseFloat(state.inputValue))
-                    
-                    return Object.assign({},state,{               
-                                    inputValue:answer,
-                                    previouseInputVal:0,
-                                    selectedOperator:null,
-                                    displayValue:answer+'',
-                                    lastOperator:action.val
-                    });
+                    if(state.inputValue*1 === 0 && state.selectedOperator === '/'){
+                        return Object.assign({},state,{
+                            previouseInputVal:state.inputValue,
+                            selectedOperator:null,
+                            displayValue:Actions.DIVIDE_BY_ZERO,
+                            inputValue:0,
+                            lastOperator:action.val
+                         });    
+                    }
+                    else{
+                        let answer=eval(parseFloat(state.previouseInputVal)+state.selectedOperator+parseFloat(state.inputValue))
+                        
+                        return Object.assign({},state,{               
+                                        inputValue:answer,
+                                        previouseInputVal:0,
+                                        selectedOperator:null,
+                                        displayValue:answer+'',
+                                        lastOperator:action.val
+                        });
+                    }
                 }
                 else{
                     return state;
@@ -54,6 +67,7 @@ const CounterReducer= (state=initialState,action)=>{
                                 displayValue:state.displayValue.includes('+') || state.displayValue.includes('-') || 
                                     state.displayValue.includes('*') || state.displayValue.includes('/') ? 
                                     state.displayValue.slice(0,state.displayValue.lastIndexOf(state.selectedOperator)+1):'',
+                                selectedOperator:state.displayValue === Actions.DIVIDE_BY_ZERO?null:state.selectedOperator,
                                 lastOperator:action.val
                 });
         case Actions.C_KEY:
@@ -65,6 +79,7 @@ const CounterReducer= (state=initialState,action)=>{
                             lastOperator:null
                 });
         case Actions.DELETE_KEY:
+        if(state.lastOperator !== Actions.EQAUL){
                 return Object.assign({},state,{ 
                             inputValue:state.displayValue !== '' && (state.displayValue.substr(-1)===state.selectedOperator)?state.previouseInputVal:state.inputValue !== 0 ? 
                             (state.inputValue+'').slice(0,-1):0,
@@ -73,6 +88,10 @@ const CounterReducer= (state=initialState,action)=>{
                             displayValue:state.displayValue!=''?state.displayValue.slice(0,-1):'',
                             lastOperator:action.val
                 });
+            }
+            else{
+                return state;
+            }
         case Actions.DOT_KEY:
                 if(state.displayValue !== '' && state.lastOperator !== '='){
                     if(state.displayValue.substr(-1) !== '.' && this.isValueInteger(state.inputValue) && this.isValid(state))
@@ -125,11 +144,7 @@ isValid=(state)=>{
 }
 
 isValueInteger=(nextVal)=>{
-    if(!isNaN(nextVal) && parseInt(Number(nextVal)) == nextVal && !isNaN(parseInt(nextVal, 10))){
-        return true;
-    }
-    else{
-        return false;
-    }
+    var regexp = /^\d+\.\d+$/;
+    return !regexp.test(nextVal);
 }
 export default CounterReducer; 
